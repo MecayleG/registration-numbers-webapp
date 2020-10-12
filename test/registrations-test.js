bbe543c481e8fc00041bfceb1e6d68d64199b000
root <root@DESKTOP-V8P4AHJ.localdomain>
2020-10-12 15:08:09 +0200

    small changes

--- test/registrations-test.js
+++ test/registrations-test.js
@@ -1,7 +1,6 @@
 const assert = require('assert');
 
 const Registrations = require("../factory/registrations");
-// const registration = require('../routes/registration');
 
 describe("The Registrations Number Webapp", function() {
     const pg = require("pg");
@@ -10,27 +9,22 @@ describe("The Registrations Number Webapp", function() {
     const pool = new Pool({
         connectionString
     });
-    const INSERT_QUERY = "insert into reg_number (reg) values ($1)";
     beforeEach(async function() {
         await pool.query("delete from reg_number");
     });
     describe("The optionSelected function", function() {
         it("should return all registration numbers in the database", async function() {
             let registrations = Registrations(pool)
+
             await registrations.adding("CA 555")
-
-            // const results = await pool.query("select reg from reg_number");
-
             assert.deepEqual([{ reg: 'CA 555' }], await registrations.optionSelected("all"));
 
         });
         it("should not add duplicate registration numbers into the database", async function() {
             let registrations = Registrations(pool)
+
             await registrations.adding("CA 555")
             await registrations.adding("CA 555")
-
-
-            // const results = await pool.query("select reg from reg_number");
 
             assert.deepEqual([{ reg: 'CA 555' }], await registrations.allTheRegs());
 
@@ -38,10 +32,10 @@ describe("The Registrations Number Webapp", function() {
 
         it("should return all registrations starting with CA", async function() {
             let registrations = Registrations(pool)
+
             await registrations.adding("CA 555");
             await registrations.adding("CA 523");
             await registrations.adding("CJ 888");
-            // const results = await pool.query("select reg from reg_number where towns_id = 1");
             assert.deepEqual([{ reg: "CA 555" }, { reg: "CA 523" }], await registrations.optionSelected("1"))
         });
         it("should return all registrations starting with CJ", async function() {
